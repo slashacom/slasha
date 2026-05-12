6e1a191fb3c69e7c4340c358a9c5aa3ea5f78b1c
Kamran Ahmed <kamranahmed.se@gmail.com>
2026-05-12 18:16:51 +0100

    feat: add MySQL, MongoDB, Redis services

--- slasha-db/src/models/service.rs
+++ slasha-db/src/models/service.rs
@@ -48,24 +48,36 @@ pub struct Service {
 #[ts(export, export_to = "./service.ts")]
 pub enum ServiceKind {
     PostgreSQL,
+    MySQL,
+    MongoDB,
+    Redis,
 }
 
 impl ServiceKind {
     pub fn docker_image(&self, version: &str) -> String {
         match self {
             ServiceKind::PostgreSQL => format!("postgres:{}", version),
+            ServiceKind::MySQL => format!("mysql:{}", version),
+            ServiceKind::MongoDB => format!("mongo:{}", version),
+            ServiceKind::Redis => format!("redis:{}", version),
         }
     }
 
     pub fn container_port(&self) -> u16 {
         match self {
             ServiceKind::PostgreSQL => 5432,
+            ServiceKind::MySQL => 3306,
+            ServiceKind::MongoDB => 27017,
+            ServiceKind::Redis => 6379,
         }
     }
 
     pub fn supported_versions(&self) -> Vec<&str> {
         match self {
             ServiceKind::PostgreSQL => vec!["17", "16", "15", "14", "13"],
+            ServiceKind::MySQL => vec!["9.0", "8.4", "8.0"],
+            ServiceKind::MongoDB => vec!["8.0", "7.0", "6.0"],
+            ServiceKind::Redis => vec!["7.4", "7.2", "7.0"],
         }
     }
 
@@ -81,12 +93,43 @@ impl ServiceKind {
                     "postgres://${{ POSTGRES_USER }}:${{ POSTGRES_PASSWORD }}@${{ SLASHA.service_container_name }}:${{ PORT }}/${{ POSTGRES_DB }}".to_string(),
                 ),
             ]),
+            ServiceKind::MySQL => HashMap::from([
+                ("MYSQL_ROOT_PASSWORD".to_string(), "mysql".to_string()),
+                ("MYSQL_USER".to_string(), "mysql".to_string()),
+                ("MYSQL_PASSWORD".to_string(), "mysql".to_string()),
+                ("MYSQL_DATABASE".to_string(), "mysql".to_string()),
+                ("PORT".to_string(), "3306".to_string()),
+                (
+                    "DATABASE_URL".to_string(),
+                    "mysql://${{ MYSQL_USER }}:${{ MYSQL_PASSWORD }}@${{ SLASHA.service_container_name }}:${{ PORT }}/${{ MYSQL_DATABASE }}".to_string(),
+                ),
+            ]),
+            ServiceKind::MongoDB => HashMap::from([
+                ("MONGO_INITDB_ROOT_USERNAME".to_string(), "mongo".to_string()),
+                ("MONGO_INITDB_ROOT_PASSWORD".to_string(), "mongo".to_string()),
+                ("MONGO_INITDB_DATABASE".to_string(), "mongo".to_string()),
+                ("PORT".to_string(), "27017".to_string()),
+                (
+                    "DATABASE_URL".to_string(),
+                    "mongodb://${{ MONGO_INITDB_ROOT_USERNAME }}:${{ MONGO_INITDB_ROOT_PASSWORD }}@${{ SLASHA.service_container_name }}:${{ PORT }}/${{ MONGO_INITDB_DATABASE }}?authSource=admin".to_string(),
+                ),
+            ]),
+            ServiceKind::Redis => HashMap::from([
+                ("PORT".to_string(), "6379".to_string()),
+                (
+                    "DATABASE_URL".to_string(),
+                    "redis://${{ SLASHA.service_container_name }}:${{ PORT }}".to_string(),
+                ),
+            ]),
         }
     }
 
     pub fn volume_mount_path(&self) -> &'static str {
         match self {
             ServiceKind::PostgreSQL => "/var/lib/postgresql/data",
+            ServiceKind::MySQL => "/var/lib/mysql",
+            ServiceKind::MongoDB => "/data/db",
+            ServiceKind::Redis => "/data",
         }
     }
 }
