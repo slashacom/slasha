2be1124d2d690f7be242b1d3b057de76714fcf5c
humblepenguin <humblepenguinn@gmail.com>
2026-08-31 14:16:55 +0500

    fix: implement general ok response deserializer

--- crates/slasha-cli/src/commands/responses.rs
+++ crates/slasha-cli/src/commands/responses.rs
@@ -3,9 +3,17 @@ use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
 use slasha_db::models::logs::LogRecord;
 
-#[derive(Deserialize, Serialize)]
-pub struct OkResponse {
-    pub ok: bool,
+#[derive(Debug, Default, Clone, Copy, Serialize)]
+pub struct OkResponse;
+
+impl<'de> Deserialize<'de> for OkResponse {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let _ = serde::de::IgnoredAny::deserialize(deserializer)?;
+        Ok(OkResponse)
+    }
 }
 
 #[derive(Deserialize, Serialize)]
